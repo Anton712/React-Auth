289301e58b90ed96ed1a13dd85ec06c4d16ff300
anton <anton.loncar@outlook.com>
2025-08-08 11:17:33 +0200

    attached auth token to outgoing requests

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -37,6 +37,10 @@ export async function action({request}) {
     throw new Response('Could not authenticate user.', {status: 500});
   }
 
-  // manage that token
+  const resData = await response.json();
+  const token = resData.token;
+
+  localStorage.setItem('token', token);
+
   return redirect("/");
 }
